c2d422e7e1fd9a30ae1177a436976aa376c6e7e4
Jim Shaw <jimxshaw@gmail.com>
2021-08-21 12:13:03 -0500

    Essentials: add TypeIntro sample.

---
+++ Essentials/HelloWorld/src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,18 @@
+const val MAX_EXPERIENCE = 10000
+
+fun main() {
+    val playerName = "James"
+    var experiencePoints = 5
+
+    var hasSteed = false
+    val pub = "Unicorn's Horn"
+    var pubServer = "Leon"
+    var goldAmount = 50
+    var menu = listOf("wine", "mead", "LaCroix")
+
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+
+
+}
