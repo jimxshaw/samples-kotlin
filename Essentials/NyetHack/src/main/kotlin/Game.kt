9e9c0263d0eccafaccb8a6eb2adb444423b48a24
Jim Shaw <jimxshaw@gmail.com>
2021-08-22 13:45:59 -0500

    Essentials: update files and classes for NyetHack.

--- Essentials/NyetHack/src/main/kotlin/Game.kt
+++ Essentials/NyetHack/src/main/kotlin/Game.kt
@@ -1,54 +1,13 @@
 fun main() {
-    val name = "Madrigal"
-    var healthPoints = 89
-    val isBlessed = true
-    val isImmortal = false
-
-    // Aura
-    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
-
-    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    val player = Player()
+    player.castFireball()
 
     // Player status
-    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
-
-    castFireball()
+    printPlayerStatus(player)
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String =
-    when (healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-            "has some minor wounds, but is healing quite quickly!"
-        } else {
-            "has some minor wounds."
-        }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
-
-private fun printPlayerStatus(
-    auraColor: String,
-    isBlessed: Boolean,
-    name: String,
-    healthStatus: String
-) {
-    println("(Aura: $auraColor) " +
-            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name $healthStatus")
-}
-
-private fun auraColor(
-    isBlessed: Boolean,
-    healthPoints: Int,
-    isImmortal: Boolean
-): String {
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    return auraColor
-}
-
-private fun castFireball(numFireballs: Int = 2) {
-    println("A glass of fireball springs into existence. (x$numFireballs)")
+private fun printPlayerStatus(player: Player) {
+    println("(Aura: ${player.auraColor()}) " +
+            "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} ${player.formatHealthStatus()}")
 }
