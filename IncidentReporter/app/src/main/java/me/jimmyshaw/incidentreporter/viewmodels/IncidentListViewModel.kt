80d6729876b06aa0765744ce8ef4b6bf33d3e6c8
Jim Shaw <jimxshaw@gmail.com>
2021-08-24 12:39:42 -0500

    Add IncidentListViewModel class with faux data.

---
+++ IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/viewmodels/IncidentListViewModel.kt
@@ -0,0 +1,18 @@
+package me.jimmyshaw.incidentreporter.viewmodels
+
+import androidx.lifecycle.ViewModel
+import me.jimmyshaw.incidentreporter.models.Incident
+
+class IncidentListViewModel : ViewModel() {
+    val incidents = mutableListOf<Incident>()
+
+    init {
+        for (i in 0 until 50) {
+            val incident = Incident()
+
+            incident.description = "Incident $i"
+            incident.isResolved = i % 2 == 0
+            incidents.add(incident)
+        }
+    }
+}
