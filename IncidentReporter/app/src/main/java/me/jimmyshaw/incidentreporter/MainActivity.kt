2a54c2c357a59b9fd5e0f5b5d3c402fa8f9ad128
Jim Shaw <jimxshaw@gmail.com>
2021-08-24 11:12:05 -0500

    IncidentReporter: update MainActivity to use IncidentFragment.

--- IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/MainActivity.kt
+++ IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/MainActivity.kt
@@ -2,10 +2,23 @@ package me.jimmyshaw.incidentreporter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.jimmyshaw.incidentreporter.fragments.IncidentFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = IncidentFragment()
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
