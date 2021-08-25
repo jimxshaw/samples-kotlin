33c32bbc4831600c299a6475b24d32641c8b0164
Jim Shaw <jimxshaw@gmail.com>
2021-08-24 22:47:38 -0500

    Update IncidentListFragment to work with LiveData.

--- IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/fragments/IncidentListFragment.kt
+++ IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/fragments/IncidentListFragment.kt
@@ -9,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,7 +22,11 @@ private const val TAG = "IncidentListFragment"
 class IncidentListFragment : Fragment() {
 
     private lateinit var incidentRecyclerView: RecyclerView
-    private var adapter: IncidentAdapter? = null
+
+    // Initialize with an empty list because this fragment will
+    // have to wait for the database results before it
+    // can populate the recycler view.
+    private var adapter: IncidentAdapter? = IncidentAdapter(emptyList())
 
     private val incidentListViewModel: IncidentListViewModel by lazy {
         ViewModelProvider(this).get(IncidentListViewModel::class.java)
@@ -33,11 +38,6 @@ class IncidentListFragment : Fragment() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total incidents: ${incidentListViewModel.incidents.size}")
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -47,14 +47,33 @@ class IncidentListFragment : Fragment() {
 
         incidentRecyclerView = view.findViewById(R.id.rv_incident) as RecyclerView
         incidentRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
+        incidentRecyclerView.adapter = adapter
 
         return view
     }
 
-    private fun updateUI() {
-        val incidents = incidentListViewModel.incidents
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // This observe function is used to register an observer on the LiveData
+        // instance and tie the life of the observation to the life of this fragment.
+        incidentListViewModel.incidentListLiveData.observe(
+            // Observe the LiveData to ensure that the view is ready to display
+            // the incident data. Using the view's lifecycle owner ensures we
+            // will not receive updates when the view is not on the screen.
+            viewLifecycleOwner,
+            // This Observer implementation is responsible for
+            // reacting to new data from the LiveData.
+            Observer { incidents ->
+                incidents?.let {
+                    Log.i(TAG, "Got incidents ${incidents.size}")
+                    updateUI(incidents)
+                }
+            }
+        )
+    }
+
+    private fun updateUI(incidents: List<Incident>) {
         adapter = IncidentAdapter(incidents)
         incidentRecyclerView.adapter = adapter
     }
