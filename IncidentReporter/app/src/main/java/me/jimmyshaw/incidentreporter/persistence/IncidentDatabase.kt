7d3c7f390a6de4423a8d3e1a597919cabf35989a
Jim Shaw <jimxshaw@gmail.com>
2021-08-24 21:04:22 -0500

    Add IncidentTypeConverters to IncidentDatabase class.

--- IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/persistence/IncidentDatabase.kt
+++ IncidentReporter/app/src/main/java/me/jimmyshaw/incidentreporter/persistence/IncidentDatabase.kt
@@ -2,8 +2,10 @@ package me.jimmyshaw.incidentreporter.persistence
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import me.jimmyshaw.incidentreporter.models.Incident
 
 @Database(entities = [Incident::class], version = 1)
+@TypeConverters(IncidentTypeConverters::class)
 abstract class IncidentDatabase : RoomDatabase() {
 }
