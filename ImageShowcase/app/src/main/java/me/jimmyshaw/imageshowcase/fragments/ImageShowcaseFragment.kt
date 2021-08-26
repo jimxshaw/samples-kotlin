94ac8f0b2b6e2c227436d318105eb178a0753173
Jim Shaw <jimxshaw@gmail.com>
2021-08-26 11:17:23 -0500

    Add network call to onCreate ImageShowcaseFragment.

--- ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/fragments/ImageShowcaseFragment.kt
+++ ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/fragments/ImageShowcaseFragment.kt
@@ -1,6 +1,7 @@
 package me.jimmyshaw.imageshowcase.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,8 +10,13 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import me.jimmyshaw.imageshowcase.R
 import me.jimmyshaw.imageshowcase.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "ImageShowcaseFragment"
 
 class ImageShowcaseFragment : Fragment() {
 
@@ -29,6 +35,21 @@ class ImageShowcaseFragment : Fragment() {
             .build()
 
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrHomePageRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(
+                call: Call<String>,
+                response: Response<String>
+            ) {
+                Log.d(TAG, "Response received: ${response.body()}")
+            }
+        })
     }
 
     override fun onCreateView(
