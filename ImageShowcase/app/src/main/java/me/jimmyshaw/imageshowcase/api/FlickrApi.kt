239c4cc123545b3e49e06beb77b83e8a185cfaf8
Jim Shaw <jimxshaw@gmail.com>
2021-08-26 15:53:28 -0500

    Update FlickrApi to use api key from secrets xml that's not checked into source control.

--- ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/api/FlickrApi.kt
+++ ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/api/FlickrApi.kt
@@ -1,14 +1,15 @@
 package me.jimmyshaw.imageshowcase.api
 
+import android.content.res.Resources
+import me.jimmyshaw.imageshowcase.R
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
 
-    @GET("services/rest/?method=flickr.interestingness.getList"
-            + "&api_key=yourApiKeyHere"
-            + "&format=json"
-            + "&nojsoncallback=1"
-            + "&extras=url_s")
-    fun fetchImages(): Call<String>
+    @GET
+    fun fetchImages(
+        @Url url: String = Resources.getSystem().getString(R.string.flickr_get_images_url)
+    ): Call<String>
 }
