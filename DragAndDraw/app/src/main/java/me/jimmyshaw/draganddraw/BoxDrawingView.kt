5ba6213c1c7de7714406b743288fd84d7a231e8f
Jim Shaw <jimxshaw@gmail.com>
2021-08-27 11:33:00 -0500

    Initial commit for Drag And Draw sample app.

---
+++ DragAndDraw/app/src/main/java/me/jimmyshaw/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,11 @@
+package me.jimmyshaw.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+private const val TAG = "BoxDrawingView"
+
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+}
