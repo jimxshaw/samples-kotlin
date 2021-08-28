6ef47ab09d8e2063145b4e208f5b0923364c401b
Jim Shaw <jimxshaw@gmail.com>
2021-08-27 20:59:38 -0500

    Update ShowcaseItem data class with SerializedName annotation to map JSON data to this model.

--- ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/models/ShowcaseItem.kt
+++ ImageShowcase/app/src/main/java/me/jimmyshaw/imageshowcase/models/ShowcaseItem.kt
@@ -1,7 +1,9 @@
 package me.jimmyshaw.imageshowcase.models
 
+import com.google.gson.annotations.SerializedName
+
 data class ShowcaseItem(
     var title: String = "",
     var id: String = "",
-    var url: String = ""
+    @SerializedName("url_s") var url: String = ""
 )
